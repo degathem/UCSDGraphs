2d1f354f9ed95c059e047a272ef46da5249024aa
Cam Gillespie <gillespie.cam@gmail.com>
2016-04-17 16:45:35 -0600

    Created RoadEdge class

--- src/roadgraph/RoadEdge.java
+++ src/roadgraph/RoadEdge.java
@@ -1,5 +1,38 @@
 package roadgraph;
 
-public class RoadEdge {
+import geography.GeographicPoint;
 
+public class RoadEdge {
+	private GeographicPoint to;
+	private String roadName;
+	private String roadType;
+	private double length;
+	
+	public void setRoadName(String roadName){
+		this.roadName = roadName;
+	}
+	
+	public void setRoadType(String roadType){
+		this.roadType = roadType;
+	}
+	
+	public void setLength(double length){
+		this.length = length;
+	}
+	
+	public String getRoadName(){
+		return roadName;
+	}
+	
+	public String getRoadType(){
+		return this.roadType;
+	}
+	
+	public double getRoadLength(){
+		return this.length;
+	}
+	
+	public GeographicPoint GeographicPoint() {
+		return null;
+	}
 }
